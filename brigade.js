171987ec127cf66a5f2e1ce37c58a5a763a60aca
gogsadmin <RedHat$1>
2018-07-29 17:43:11 +0200

    initial commit

---
+++ brigade.js
@@ -0,0 +1,16 @@
+const { events, Job } = require("brigadier")	
+  	
+events.on("getpods", (brigadeEvent, project) => {	
+  console.log("==> handling 'cmdemo' job")	
+  const docker = new Job("cmdemo", "cvugrinec/azcli-kubectl")	
+  docker.env= {"USERNAME": project.secrets.username}
+  docker.env= {"PASSWORD": project.secrets.password}
+  docker.env= {"TENANT": project.secrets.tenant}
+  docker.privileged = true;	
+  docker.tasks = [	
+    "az login --service-principal --username $USERNAME --password $PASSWORD --tenant $TENANT",	
+    "kubectl get pods",	
+  ];	
+   docker.run()	
+   console.log("==> finished 'cmdemo' job")	
+})
