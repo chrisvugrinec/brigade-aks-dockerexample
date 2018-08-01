84c27393d12221311a85a415594099b79d03374c
gogsadmin <RedHat$1>
2018-08-01 02:54:40 +0200

    added mutliple job

--- brigade.js
+++ brigade.js
@@ -1,18 +1,43 @@
 const { events, Job } = require("brigadier")	
   	
-events.on("getpods", (brigadeEvent, project) => {	
+events.on("dodemo", (brigadeEvent, project) => {	
+
   console.log("==> handling 'cmdemo' job")	
-  const docker = new Job("cmdemo", "cvugrinec/azcli-kubectl")	
-  docker.env = {
+
+  // List Pod Job
+  const listPodJob = new Job("cmdemo-listpods", "cvugrinec/azcli-kubectl")	
+  listPodJob.env = {
+    "USERNAME": project.secrets.username, 
+    "PASSWORD": project.secrets.password,
+    "TENANT": project.secrets.tenant 
+  }
+  listPodJob = true;	
+  listPodJob.tasks = [	
+    "az login --service-principal --username $USERNAME --password $PASSWORD --tenant $TENANT",	
+    "kubectl get pods",	
+  ];	
+
+  // Burst Job, using ACI connector plugin...expanding cluster with ACI nodes
+  const burstJob = new Job("cmdemo-burstjob", "cvugrinec/azcli-kubectl")
+  listPodJob.env = {
     "USERNAME": project.secrets.username,
     "PASSWORD": project.secrets.password,
     "TENANT": project.secrets.tenant
   }
-  docker.privileged = true;	
-  docker.tasks = [	
-    "az login --service-principal --username $USERNAME --password $PASSWORD --tenant $TENANT",	
-    "kubectl get pods",	
-  ];	
-  docker.run()	
+  burstJob.tasks = [
+    "az login --service-principal --username $USERNAME --password $PASSWORD --tenant $TENANT",
+    "git clone https://github.com/chrisvugrinec/busybox-example.git",
+    "cd busybox-example.git",
+    "k create -f busybox.yaml"
+  ];
+
+
+  // Example of piping of jobs...first listPodJob...and then burstJob
+  listPodJob.run().then(() => {
+    burstJob.run()
+  })
+
+
   console.log("==> finished 'cmdemo' job")	
 })
+
